276bb467349d044deee7782e058dc553d2fef13d
Juan Carlos <jcrequena.fp@gmail.com>
2019-12-09 16:34:44 +0100

    Update algoritmo.c

--- UD5/algoritmo.c
+++ UD5/algoritmo.c
@@ -5,32 +5,37 @@ int main(int argc, char *argv[])
 {
 float x,y,s,r,p,d;
 clock_t start, end;
-double cpu_time_used;
+float cpu_time_used;
+int cont;
 
-
-printf("Operaciones básicas entre dos números\n");
-printf("Introduce el número x:");
-scanf("%f", &x);
-printf("Introduce el número y:");
-scanf("%f",&y);
-//Tiempo al inicio
-start = clock();
-
-s = x+y;
-r = x-y;
-p = x*y;
-d = x/y;
-
+	printf("Operaciones básicas entre dos números\n");
+	printf("Introduce el número x:");
+	scanf("%f", &x);
+	printf("Introduce el número y:");
+	scanf("%f",&y);
+	s=0;
+	r=0;
+	p=0;
+	d=0;
+	//Tiempo al inicio
+	start = clock();
+	for (cont=1;cont<=100000;cont=cont+1)
+	{
+    s = s + (x+y);
+	  r = r + (x-y);
+	  p = p + (x*y);
+	  d = d + (x/y);
+	}
 //Tiempo al fin
 end = clock();
 
 //Mostrar resultados por pantalla
 //
 printf("---Resultados de las operaciones matemáticas básicas---\n");
-printf("El resultado de x+y es:%f\n",s);
-printf("El resultado de x-y es: %f\n",r);
-printf("El resultado de x*y es: %f\n",p);
-printf("El resultado de x/y es: %f\n",d);
+printf("El resultado de s + (x+y) es:%f\n",s);
+printf("El resultado de r + (x-y) es: %f\n",r);
+printf("El resultado de p + (x*y) es: %f\n",p);
+printf("El resultado de d + (x/y) es: %f\n",d);
 
 printf("--Resultados computacionales---\n");
 //CLOCKS_PER_SECOND : the number of clock ticks per second
@@ -42,7 +47,7 @@ printf("Ending Time:%lu\n",end);
 printf("CLOCKS_PER_SEC:%lu",CLOCKS_PER_SEC);
 printf("\nNumber of clock ticks:%lu\n",(end - start));
 
-cpu_time_used = (double)(end - start)/CLOCKS_PER_SEC;
-//Tiempo transcurrido durante la realización de las 4 operaciones matemáticas
-printf("Tiempo transcurrido: %g segundos\n",cpu_time_used);
+cpu_time_used = (float)(end - start)/CLOCKS_PER_SEC;
+//Tiempo transcurrido durante la realización de las 4 * 100.000 operaciones matemáticas
+printf("Tiempo transcurrido: %f segundos\n",cpu_time_used);
 }
